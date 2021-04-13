d3316828b871eff4e792131d75bcce62f9cf63fa
Yilmaz Ádám Timur <ptcadam@gmail.com>
2021-04-13 12:02:21 +0200

    main methods

--- src/main/Java/com/horseman/polish/Game.java
+++ src/main/Java/com/horseman/polish/Game.java
@@ -1,17 +1,42 @@
 package com.horseman.polish;
 
+import com.sun.tools.jdeprscan.scan.Scan;
+
+import java.util.Scanner;
+
 public class Game {
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
+        int n;
+
+        while(true){
+            System.out.print("Board size between 10-20: ");
+            n = scanner.nextInt();
+            //check valid input
+            if (n > 20 || n < 10){
+                System.out.println("Size out of range");
+            }else{
+                //break out of loop and continue to create the board
+                break;
+            }
+        }
+
+        //Start();
+    }
+
+    static void Start(int[][] board){
+        //starts game between players
+    }
+
     //Welcome message
     //Ask user for board size(between 10-20)*Handle out of range
 
-    //Start(); * start game between players
 
     //playRound(); *Checks who's playing *Checks if there's a winner
 
     //Checks(); *Checks if valid position from user and within board
     // if yes, tryToMakeMove() called on pawn instance
     //checkForWinner() * Check after each round of valid play, also check for draw!
-    public static void main(String[] args) {
-
-    }
 }
