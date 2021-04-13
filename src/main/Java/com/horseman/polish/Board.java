c3671b58e9410fa863b24bdc1b40a60f14e49268
Yilmaz Ádám Timur <ptcadam@gmail.com>
2021-04-13 11:33:15 +0200

    basic board constructor

--- src/main/Java/com/horseman/polish/Board.java
+++ src/main/Java/com/horseman/polish/Board.java
@@ -1,4 +1,19 @@
 package com.horseman.polish;
 
 public class Board {
+    private int[][] board;
+
+    public Board(int n){
+        int[][] board = new int[n][n];
+
+    }
+
+    public int[][] setBoard(int[][] board){
+    return this.board = board;
+    }
+
+    public int[][] getBoard(){
+    return board;
+    }
+
 }
