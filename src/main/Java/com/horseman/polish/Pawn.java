d5bdd2ec58c9f6fecc77e0cb947472ce3ee256b3
tothgeza <toth.geza.0425@gmail.com>
2021-04-13 11:38:08 +0200

    Implement getIsWhite method

--- src/main/Java/com/horseman/polish/Pawn.java
+++ src/main/Java/com/horseman/polish/Pawn.java
@@ -1,4 +1,20 @@
 package com.horseman.polish;
 
 public class Pawn {
+
+    private String color;
+
+    Pawn() {
+        this.setColor("white");
+    }
+    Pawn(String color) {
+        this.setColor("black");
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public boolean getIsWhite() {
+        return color.equals("white");
+    }
 }
