ec6026d2029de5bd43870b904cb890b5a8826a41
tothgeza <toth.geza.0425@gmail.com>
2021-04-13 12:31:52 +0200

    Add Coordinates class

--- src/main/Java/com/horseman/polish/Pawn.java
+++ src/main/Java/com/horseman/polish/Pawn.java
@@ -4,17 +4,34 @@ public class Pawn {
 
     private String color;
 
-    Pawn() {
-        this.setColor("white");
-    }
-    Pawn(String color) {
+    Pawn(String color, int x, int y) {
         this.setColor("black");
+        Coordinates.x = x;
+        Coordinates.y = y;
     }
 
     public void setColor(String color) {
         this.color = color;
     }
+    public String getColor() {
+        return this.color;
+    }
     public boolean getIsWhite() {
         return color.equals("white");
+    }
+
+    public void setCoordinates(int x, int y) {
+        Coordinates.x = x;
+        Coordinates.y = y;
+    }
+
+    public int[] getCoordinates() {
+        return new int[]{Coordinates.x, Coordinates.y};
+    }
+
+    static class Coordinates {
+        public static int x;
+        public static int y;
+
     }
 }
