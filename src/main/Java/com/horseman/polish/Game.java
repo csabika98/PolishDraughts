3458cd26912dd3048ea90d8db8dddba0e663893c
Yilmaz Ádám Timur <ptcadam@gmail.com>
2021-04-13 11:50:09 +0200

    game logic scetch

--- src/main/Java/com/horseman/polish/Game.java
+++ src/main/Java/com/horseman/polish/Game.java
@@ -1,5 +1,17 @@
 package com.horseman.polish;
 
 public class Game {
+    //Welcome message
+    //Ask user for board size(between 10-20)*Handle out of range
 
+    //Start(); * start game between players
+
+    //playRound(); *Checks who's playing *Checks if there's a winner
+
+    //Checks(); *Checks if valid position from user and within board
+    // if yes, tryToMakeMove() called on pawn instance
+    //checkForWinner() * Check after each round of valid play, also check for draw!
+    public static void main(String[] args) {
+
+    }
 }
