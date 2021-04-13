585b0f4d4e77e8730914cf68dcf4bb81e52c879b
Yilmaz Ádám Timur <ptcadam@gmail.com>
2021-04-13 14:09:13 +0200

    OOP fix

--- src/main/Java/com/horseman/polish/Board.java
+++ src/main/Java/com/horseman/polish/Board.java
@@ -1,13 +1,28 @@
 package com.horseman.polish;
 
 public class Board {
-    String pawn_1 = "X";
-    String pawn_2 = "O";
-    static String[] letters = {" ", "A", "B", "C", "D", "E", "F", "G", "H", "I","J","K","L","M","N","O"};
+    private int[][] board;
+
+    public Board(int n) {
+        int[][] board = new int[n][n];
+        this.setBoard(board);
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void setBoard(int[][] board) {
+        this.board = board;
+    }
     // Size can upto 15
-    public static void displayBoard(int size) {
-        for (int i = 0; i <= size; i++) {
-            for (int j = 0; j <= size; j++) {
+    public void displayBoard() {
+        String pawn_1 = "X";
+        String pawn_2 = "O";
+        String[] letters = {" ", "A", "B", "C", "D", "E", "F", "G", "H", "I","J","K","L","M","N","O"};
+
+        for (int i = 0; i <= board[0].length; i++) {
+            for (int j = 0; j <= board.length; j++) {
                 System.out.print(i == 0 || j == 0 ? i == 0 ? letters[j] : letters[i] : "-"); //trenary :)
                 System.out.print(" ");
             }
