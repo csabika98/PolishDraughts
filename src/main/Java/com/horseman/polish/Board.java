a62360c951f18257dda6019168c5cbe23b966020
csabika98 <6ix9inegotti@naver.com>
2021-04-15 08:44:49 +0200

    refactoring & fix board to display numbers & letters

--- src/main/Java/com/horseman/polish/Board.java
+++ src/main/Java/com/horseman/polish/Board.java
@@ -46,13 +46,12 @@ public class Board {
     }
 
 
-    // Size can upto 15
+
     public void displayBoard(Pawn[][] board) {
         String[] letters = {"   ","1  ","2  ","3  ","4  ","5  ","6  ","7  ","8  ","9 ","10 ","11 ","12 ","13 ","14 ","15 ","16 ","17 ","18 ","19 ","20  "};
         for (int i =0; i<=1; i++) {
-            System.out.println("");
+            System.out.println();
             for (int j = 0; j <= board.length; j++) {
-                //System.out.println();
                 if (i == 0) {
                     System.out.print(letters[j]);
                 }
